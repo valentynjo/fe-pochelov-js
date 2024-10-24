31dc7e836e6f04f614505b84c441d82b6025f1b4
valentynjo <rediska.valik@gmail.com>
2024-10-24 16:20:14 +0200

    start

---
+++ 1/script.js
@@ -0,0 +1,17 @@
+let userNickname = 'Username';
+let age = 23;
+console.log(age);
+age = age +5;
+console.log(age);
+
+let isStudent = true;
+console.log(`Is student ${isStudent}`);
+isStudent = false;
+console.log(`Is student ${isStudent}`);
+
+let item = 10;
+console.log(`Item ${item}`);
+item = 'ten';
+console.log(`Item ${item}`);
+item = true;
+console.log(`Item ${item}`);
