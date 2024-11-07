ef8fa6a4ba91f3dc4f6b07d89f1159168540f599
valentynjo <rediska.valik@gmail.com>
2024-11-07 19:40:57 +0100

    game

--- 13.all/game/script.js
+++ 13.all/game/script.js
@@ -2,7 +2,6 @@ function playGame(userChoice) {
     const options = ["камінь", "ножиці", "папір"];
     const computerChoice = options[Math.floor(Math.random() * options.length)];
     
-    // Логіка визначення результату
     let resultMessage = `Ви обрали: ${userChoice}<br>Комп'ютер обрав: ${computerChoice}<br>`;
     if (userChoice === computerChoice) {
         resultMessage += "Нічия!";
@@ -16,6 +15,5 @@ function playGame(userChoice) {
         resultMessage += "Ви програли!";
     }
 
-    // Відображення результату
     document.getElementById("result").innerHTML = resultMessage;
 }
